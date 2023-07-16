ae7418335cd76347aaa186897ffe477f795bf631
paoloflore99 <paoloflore937@gmail.com>
2023-07-17 00:18:40 +0200

    fine lista

---
+++ myscript.js
@@ -0,0 +1,54 @@
+"use strict"
+
+//lista team------------------------
+const listaTeam = [
+    {
+        nome: "Wayne",
+        cognome: "Barnett",
+        ruolo: "Founder & CEO",
+        Foto: "wayne-barnett-founder-ceo.jpg",
+    },
+
+    {
+        nome: "Angela",
+        cognome: "Caroll",
+        ruolo: "Chief Editor",
+        Foto: "angela-caroll-chief-editor.jpg",
+    },
+
+    {
+        nome: "Walter",
+        cognome: "Gordon",
+        ruolo: "Office Manager",
+        Foto: "walter-gordon-office-manager.jpg",
+    },
+
+    {
+        nome: "Angela",
+        cognome: "Lopez",
+        ruolo: "Social Media Manager",
+        Foto: "angela-lopez-social-media-manager.jpg",
+    },
+
+    {
+        nome: "Scott",
+        cognome: "Estrada",
+        ruolo: "Developer",
+        Foto: "scott-estrada-developer.jpg",
+    },
+
+    {
+        nome: "Barbara",
+        cognome: "Ramos",
+        ruolo: "Graphic Designer",
+        Foto: "barbara-ramos-graphic-designer.jpg",
+    },
+
+    
+]
+
+console.log(listaTeam);
+
+
+//se invece come valore voglio mettere un numero non lo metto dentro le virgolette
+//-------------------------------------------------------------
